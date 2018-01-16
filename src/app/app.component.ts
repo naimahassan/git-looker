967a7e3837d3a433a16a56bfab29e90a7a25c1cb
naimahassan <hassannaima77@gmail.com>
2018-01-16 10:19:09 +0300

    spacing error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,30 @@
 import { Component } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { GithubService } from './githubs/github.service'
 
 @Component({
- selector: 'app-root',
- templateUrl: './app.component.html',
- styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- constructor(private http:HttpClient,private _githubService:GithubService){}
- userName:any=[];
- githubData:any=[];
- //avatarUrl="./assets/imageholder.png";
+  constructor(private http: HttpClient, private _githubService: GithubService) { }
+  userName: any = [];
+  githubData: any = [];
+  //avatarUrl="./assets/imageholder.png";
 
- ngOnInit(){
-   this._githubService.getUser().subscribe(userName => {
+  ngOnInit() {
+    this._githubService.getUser().subscribe(userName => {
       //console.log(userName)
       this.userName = userName
-   })
- }
+    })
+  }
 
-//  searchUser(){
-  
-//  }
+  //  searchUser(){
 
-} 
+  //  }
+
+}
 
 
 
